6818e110546feba5c4486ec94d223efa98c5e764
shanto <shantoshaji92@gmail.com>
2015-07-22 08:48:37 +0530

    shanto

--- shanto/app/app/scripts/controllers/main.js
+++ shanto/app/app/scripts/controllers/main.js
@@ -8,24 +8,31 @@
  * Controller of the loginApp
  */
 angular.module('loginApp')
-  .controller('MainCtrl',['$scope','myService','$window', function ($scope,myService,$window) {
+  .controller('MainCtrl',['$scope','myService','$window','localStorageService', function ($scope,myService,$window,localStorageService) {
+if (localStorageService.get('login')) {
+  $window.location.href = '#/Home';
+};
+  $scope.login = [];
+  var loginInStore = localStorageService.get('login');
+  $scope.login = loginInStore || [];
 $scope.login=function(){
+
 var email=$scope.email;
 var pass =$scope.password;
 var promise=myService.loginservice(email,pass)
     promise.then(function(response) {
-      console.log(response.data)
        if(response.data=="false"){
 
          alert("invalid login");
 
        }
        else{
-         $window.location.href = '#/Home';
+        data=response.data;
+        localStorageService.set('login',data);
+        $window.location.href = '#/Home';
        }
 
-    })
+    }, true)
 
 }
   }]);
- 
